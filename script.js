9df073cb28569d58d76995ac577ac0449989bef8
jolson615 <jolson615@gmail.com>
2018-10-02 12:03:06 -0400

    new grid

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let main = document.querySelector("#mainContainer")
 let scoreElement = document.querySelector("#score")
 var score = 0
 
-let nums = Array.from({length: 20}, () => Math.floor(Math.random() * 100)).sort((a, b) => a - b);
+let nums = Array.from({length: 21}, () => Math.floor(Math.random() * 100)).sort((a, b) => a - b);
 // Create a times function
 const times = x => f => {if (x > 0) { f(); times (x - 1) (f)}}
 // Call the times function to create all the on-screen elements
 var i = 0
-times (20) (() => {main.innerHTML += `<div class="item grey"><div>Idx: ${i}</div><div class="number hidden">${nums[i]}</span></div>`, i+=1})
+times (21) (() => {main.innerHTML += `<div class="item grey"><div>Idx: ${i}</div><div class="number hidden">${nums[i]}</span></div>`, i+=1})
 // Create an array of random numbers
 
 let children = document.querySelectorAll(".item")
